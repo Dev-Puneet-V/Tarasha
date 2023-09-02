bf19654959f45d197b07e83080322cc6fc7a418d
Puneet-iner <imhtcy312@gmail.com>
2023-09-02 20:40:09 +0530

    improved package

--- src/components/Package/index.tsx
+++ src/components/Package/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { BsArrowUpRight } from 'react-icons/bs';
+import { FaBeer } from 'react-icons/fa';
 import TickIcon from '../../assets/tick.png';
 import { Packageinterface } from '../../utils/type';
 import './style.css';
-
 const Package: React.FC<Packageinterface> = (props) => {
     const {
         type,
@@ -10,8 +11,10 @@ const Package: React.FC<Packageinterface> = (props) => {
     } = props;
 
   return (
-    <div className='package-container p-4 flex flex-col gap-3 pr-8'>
+    <div className='package-container p-4 flex flex-col pr-8'>
         <p className='text-styled-2'>{type}</p>
+        <div className='package-divider mb-8'/>
+        <div className='flex flex-col gap-3'>
         {
             features?.map((feature: string) => {
                 return (
@@ -22,6 +25,18 @@ const Package: React.FC<Packageinterface> = (props) => {
                 )
             })
         }
+        </div>
+        <div className='flex items-center gap-2 absolute select-plan text-styled'>
+            <div>
+                <p>
+                {'Select ' + type}     
+                </p>
+                <div className='package-divider select-package-divider'/>
+            </div>
+            <div className='mt-1'>
+                <BsArrowUpRight/>
+            </div>
+        </div>
     </div>
   )
 }

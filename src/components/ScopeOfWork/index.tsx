e8577fbeed75390f05a0bfbee91437ec2ec34300
Puneet-iner <imhtcy312@gmail.com>
2023-09-30 08:31:13 +0530

    scope of work

--- src/components/ScopeOfWork/index.tsx
+++ src/components/ScopeOfWork/index.tsx
@@ -1,78 +1,241 @@
 import React from 'react'
-import ChairImage from '../../assets/chair.png';
+import ChairImage from '../../assets/circular-chair.png';
 import JoinerImage from '../../assets/joiner.png';
+import useScreenSize from '../..//hooks/useMediaQuery';
 import './style.css';
 
 const ScopeOfWork: React.FC = () => {
+ const screenSize = useScreenSize();
   return (
-    <div className='p-8 flex flex-col gap-5 items-center' id='scope-of-work'>
-        <p className='text-styled'>Scope Of Work</p>
-        <div className='flex wrap justify-center gap-5 scope-container relative'>
-            {
-                [1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => {
-                    return (
-                        <div className='scope flex flex-col gap-1 justify-center mt-4 ml-8'>
-                            <div className='flex flex-col gap-1 element-container justify-center'>
-                                <div className='flex flex-col gap-1 '>
-                                    <div className='in-e-c flex justify-center items-center'>
-                                        <img className='element-img' src={ChairImage}/>
-                                    </div>
-                                </div>
-                            </div>
-                            <p className=''>Consultation</p>
-                            <p>Consultation and proper documentation to assess client’s needs and
-what we can offer.</p>
-                        </div>
-                    )
-                })
-            }
-            <div className='connector absolute'>
-                <img src={JoinerImage}/>
-                <img src={JoinerImage}/>
-                <img src={JoinerImage}/>
-                <img src={JoinerImage}/>
-                <img src={JoinerImage}/>
-                <img src={JoinerImage}/>
-                <img src={JoinerImage}/>
-                <img src={JoinerImage}/>
+    <>
+       {screenSize === 'lg' && <div className='flex flex-col gap-5 items-center' id='scope-of-work'>
+            <p className='text-styled'>Scope Of Work</p>
+            <div className='flex wrap justify-center gap-5 scope-container relative'>
+                <div className='flex flex-wrap'>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                    <img src={JoinerImage} className='joiner l-r-t-b-joiner'/>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                    <img src={JoinerImage} className='joiner l-r-b-t-joiner'/>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                </div>
             </div>
-        </div>
-        
-    </div>
+            <div className='w-75 row-12-joiner flex items-center gap-5'>
+                <img src={JoinerImage} className='joiner t-b-r-l-joiner '/>
+            </div>
+            <div className='flex wrap justify-center gap-5 scope-container relative'>
+                <div className='flex flex-wrap'>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                    <img src={JoinerImage} className='joiner r-l-t-b-joiner'/>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                    <img src={JoinerImage} className='joiner r-l-b-t-joiner'/>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='w-75 row-23-joiner flex items-center gap-5'>
+                <img src={JoinerImage} className='joiner t-b-l-r-joiner '/>
+            </div>
+            <div className='flex wrap justify-center gap-5 scope-container relative'>
+                <div className='flex flex-wrap'>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                    <img src={JoinerImage} className='joiner l-r-t-b-joiner'/>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                    <img src={JoinerImage} className='joiner l-r-b-t-joiner'/>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>}
+        {screenSize === 'md' && <div className='flex flex-col gap-5 items-center' id='scope-of-work'>
+            <p className='text-styled'>Scope Of Work</p>
+            <div className='flex wrap justify-center gap-5 scope-container relative'>
+                <div className='flex flex-wrap'>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                    <img src={JoinerImage} className='joiner l-r-t-b-joiner'/>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='w-75 row-12-joiner flex items-center gap-5'>
+                <img src={JoinerImage} className='joiner t-b-r-l-joiner '/>
+            </div>
+            <div className='flex wrap justify-center gap-5 scope-container relative'>
+                <div className='flex flex-wrap'>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                    <img src={JoinerImage} className='joiner r-l-t-b-joiner'/>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='w-75 row-23-joiner flex items-center gap-5'>
+                <img src={JoinerImage} className='joiner t-b-l-r-joiner '/>
+            </div>
+            <div className='flex wrap justify-center gap-5 scope-container relative'>
+                <div className='flex flex-wrap'>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                    <img src={JoinerImage} className='joiner l-r-t-b-joiner'/>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className='w-75 row-12-joiner flex items-center gap-5'>
+                <img src={JoinerImage} className='joiner t-b-r-l-joiner '/>
+            </div>
+            <div className='flex wrap justify-center gap-5 scope-container relative'>
+                <div className='flex flex-wrap'>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                    <img src={JoinerImage} className='joiner r-l-t-b-joiner'/>
+                    <div className='scope flex flex-col gap-1 justify-center  ml-8'>
+                        <img className='element-img' src={ChairImage}/>
+                        <p className='text-bold'>Consultation</p>
+                        <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='w-75 row-23-joiner flex items-center gap-5'>
+                <img src={JoinerImage} className='joiner t-b-l-r-joiner '/>
+            </div>
+            <div className='flex flex-wrap w-full justify-start'>
+                <div className='scope flex flex-col gap-1 justify-center ml-8 pl-8'>
+                    <img className='element-img' src={ChairImage}/>
+                    <p className='text-bold'>Consultation</p>
+                    <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                </div>
+            </div>
+        </div>}
+        {screenSize === 'sm' && <div className='flex flex-col gap-5 items-center' id='scope-of-work'>
+            <p className='text-styled'>Scope Of Work</p>
+            <div className='flex flex-wrap w-full justify-start'>
+                <div className='scope flex flex-col gap-1 justify-center '>
+                    <img className='element-img' src={ChairImage}/>
+                    <p className='text-bold'>Consultation</p>
+                    <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                </div>
+            </div>
+            <div className='w-full row-12-joiner flex items-center gap-5 justify-end'>
+                <img src={JoinerImage} className='joiner t-b-r-l-joiner '/>
+            </div>
+            <div className='flex flex-wrap w-full justify-start'>
+                <div className='scope flex flex-col gap-1 justify-center'>
+                    <img className='element-img' src={ChairImage}/>
+                    <p className='text-bold'>Consultation</p>
+                    <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                </div>
+            </div>
+            <div className='w-full row-23-joiner flex items-center gap-5 justify-start'>
+                <img src={JoinerImage} className='joiner t-b-l-r-joiner '/>
+            </div>
+            <div className='flex flex-wrap w-full justify-start'>
+                <div className='scope flex flex-col gap-1 justify-center'>
+                    <img className='element-img' src={ChairImage}/>
+                    <p className='text-bold'>Consultation</p>
+                    <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                </div>
+            </div>
+            <div className='w-full row-12-joiner flex items-center gap-5 justify-end'>
+                <img src={JoinerImage} className='joiner t-b-r-l-joiner '/>
+            </div>
+            <div className='flex flex-wrap w-full justify-start'>
+                <div className='scope flex flex-col gap-1 justify-center'>
+                    <img className='element-img' src={ChairImage}/>
+                    <p className='text-bold'>Consultation</p>
+                    <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                </div>
+            </div>
+            <div className='w-full row-23-joiner flex items-center gap-5 justify-start'>
+                <img src={JoinerImage} className='joiner t-b-l-r-joiner '/>
+            </div>
+            <div className='flex flex-wrap w-full justify-start'>
+                <div className='scope flex flex-col gap-1 justify-center'>
+                    <img className='element-img' src={ChairImage}/>
+                    <p className='text-bold'>Consultation</p>
+                    <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                </div>
+            </div>
+            <div className='w-full row-12-joiner flex items-center gap-5 justify-end'>
+                <img src={JoinerImage} className='joiner t-b-r-l-joiner '/>
+            </div>
+            <div className='flex flex-wrap w-full justify-start'>
+                <div className='scope flex flex-col gap-1 justify-center'>
+                    <img className='element-img' src={ChairImage}/>
+                    <p className='text-bold'>Consultation</p>
+                    <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                </div>
+            </div>
+            <div className='w-full row-23-joiner flex items-center gap-5 justify-start'>
+                <img src={JoinerImage} className='joiner t-b-l-r-joiner '/>
+            </div>
+            <div className='flex flex-wrap w-full justify-start'>
+                <div className='scope flex flex-col gap-1 justify-center'>
+                    <img className='element-img' src={ChairImage}/>
+                    <p className='text-bold'>Consultation</p>
+                    <p>Consultation and proper documentation to assess client’s needs and what we can offer.</p>
+                </div>
+            </div>
+            
+        </div>}
+    </>
   )
 }
 
 export default ScopeOfWork;
-
-
-// import React from 'react';
-// import './style.css'; // Import your CSS file
-
-// function CustomTable() {
-//   const rows = 10;
-//   const columns = 6;
-
-//   return (
-//     <table className="custom-table">
-//       <tbody>
-//         {Array.from({ length: rows }, (_, rowIndex) => (
-//           <tr key={rowIndex}>
-//             {
-//                 Array.from({ length: (columns / ((rowIndex % 3 === 0) ? 1 : 2)) }, (_, colIndex) => (
-//                 <td
-//                     key={colIndex}
-//                     className={`column-${(rowIndex % 3 === 0) ? 1 : 2}`}
-//                     colSpan={(rowIndex % 3 === 0) ? 1 : 2}
-//                 >
-//                     Row {rowIndex + 1}, Column {colIndex + 1}
-//                 </td>
-//                 ))
-//             }
-//           </tr>
-//         ))}
-//       </tbody>
-//     </table>
-//   );
-// }
-
-// export default CustomTable;

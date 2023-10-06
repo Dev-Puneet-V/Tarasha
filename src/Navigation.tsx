ef2acb6fd8a1e9aa264aad88701ad6eb5398f105
Puneet-iner <imhtcy312@gmail.com>
2023-10-07 00:30:32 +0530

    page not found

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Outlet, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -14,31 +14,42 @@ import SavePayment from "./pages/SavePayment";
 import DashboardUser from "./pages/DashboardUser";
 import ResetPassword from "./components/ResetPassword";
 import PrivateRoute from "./PrivateRoute";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function Navigation() {
-    return (
-        <Router basename="/">
-            <ScrollToTop />
-            <Header />
-                <Routes>
-                    <Route path="" element={<Home />}/>
-                    <Route path="our-works" element={<OurWork />} />
-                    <Route path="packages" element={<Packages />} />
-                    <Route path="services" element={<Services />} />
-                    <Route path="questionare" element={<Questionare />} />
-                    <Route
-                        path="dashboard"
-                        element={<PrivateRoute element={<DashboardUser />} />}
-                    />
-                    <Route path="save-payment" element={<SavePayment />} />
-                    <Route path="dashboard" element={<DashboardUser />} />
-                    <Route path="save-payment" element={<SavePayment />} />
-                    <Route path="blog" element={<Blog />} />
-                    <Route path="blog/:id" element={<MainBlog />} />
-                    <Route path="reset-password/:token" element={<ResetPassword />} />
-                </Routes>
-            <Outlet />
-            <Footer />
-        </Router>
-    );
+  return (
+    <Router basename="/">
+      <ScrollToTop />
+      <Routes>
+        {/* Define a layout route for pages with header and footer */}
+        <Route
+          path=""
+          element={
+            <>
+              <Header />
+              <Outlet />
+              <Footer />
+            </>
+          }
+        >
+          <Route path="" element={<Home />} />
+          <Route path="our-works" element={<OurWork />} />
+          <Route path="packages" element={<Packages />} />
+          <Route path="services" element={<Services />} />
+          <Route path="questionare" element={<Questionare />} />
+          <Route
+            path="dashboard"
+            element={<PrivateRoute element={<DashboardUser />} />}
+          />
+          <Route path="save-payment" element={<SavePayment />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="blog/:id" element={<MainBlog />} />
+          <Route path="reset-password/:token" element={<ResetPassword />} />
+        </Route>
+
+        {/* Define a route without layout for the "Not Found" page */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
 }
